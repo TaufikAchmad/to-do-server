96e316701aa9fd3121e96ffb0a149f2e99ae320f
Taufik Achmad <achmad.taufik.adnan@gmail.com>
2022-04-21 22:07:34 +0700

    make description nullable in mysql / typeorm

--- src/entity/Tasks.ts
+++ src/entity/Tasks.ts
@@ -1,5 +1,11 @@
-import { Field, Int, ObjectType } from "type-graphql";
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { Field, Int, ObjectType } from 'type-graphql';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @ObjectType()
 @Entity()
@@ -9,11 +15,11 @@ export class Tasks {
   id: number;
 
   @Field(() => String)
-  @Column({length: 100})
+  @Column({ length: 100 })
   title: string;
 
-  @Field(() => String, {nullable: true})
-  @Column({ type: 'text' })
+  @Field(() => String, { nullable: true })
+  @Column({ type: 'text', nullable: true })
   description: string;
 
   @Field(() => String)
